22b3bf3259d118fb03484829a4c1acfe3fcf7045
Benjamin Hand <gneeshot@gmail.com>
2015-06-25 16:41:17 -0500

    generated model for order products

--- db/migrate/20150625212048_create_users.rb
+++ db/migrate/20150625212048_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
-      t.boolean :admin
+      t.string :name, null: false, default: 'name'
+      t.string :email, null: false, default: 'name'
+      t.string :password_digest, null: false
+      t.boolean :admin, default: false
 
       t.timestamps null: false
     end
--- db/migrate/20150625212333_create_products.rb
+++ db/migrate/20150625212333_create_products.rb
@@ -1,11 +1,11 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string :title
-      t.string :description
-      t.integer :price
-      t.integer :inventory
-      t.string :pic
+      t.string :title, null: false, default: 'title'
+      t.string :description, null: false, default: 'description'
+      t.integer :price, null: false, default: 3000
+      t.integer :inventory, null: false, default: 10
+      t.string :pic, null: false, default: 'picture url'
 
       t.timestamps null: false
     end
