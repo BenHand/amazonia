46b8e4edad505dbeb1c785ea522f69590311cb7f
Benjamin Hand <gneeshot@gmail.com>
2015-06-27 15:41:02 -0500

    set up creating new orders and order_products using F_O_C_B

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,11 @@ class OrdersController < ApplicationController
   end
 
   def create
+    order = Order.find_or_create_by(user_id: params[:id], complete: false)
+    order_product = OrderProduct.find_or_create_by(order_id: order.id, product_id: params[:product_id])
+    order_product.quantity += 1
+    order_product.save
+    render json: order_product.inspect
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :users
   resources :orders
   resources :products
+  post '/add_to_cart/:user_id/product/:product_id', to: 'orders#create', as: 'cart'
   root to: "products#index"
 end
