23d6570d105e8eb2c199ce23cbcd3cd42f9d61fa
Benjamin Hand <gneeshot@gmail.com>
2015-06-28 11:18:19 -0500

    setup mailing and centered sign up box

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,18 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all.paginate(:page => params[:page], :per_page => 12)
-    @current_order = User.find(current_user.id).orders
-                         .where(user_id: current_user.id, complete: false)[0]
-                         .order_products.order(quantity: :asc)
-    @total_price = 0
-      @current_order.each do |item|
-        @total_price += (item.quantity * item.product.price)
-      end
+    if current_user.orders.where(complete: false).count != 0
+      @current_order = User.find(current_user.id).orders
+                           .where(user_id: current_user.id, complete: false)[0]
+                           .order_products.order(quantity: :asc)
+      @total_price = 0
+        @current_order.each do |item|
+          @total_price += (item.quantity * item.product.price)
+        end
+      @orders_found = true
+    else
+      @orders_found = false
+     end
   end
 
   def new
